cfae856c51aa3e8c90c4ff481032774efdcd83ef
Veronika Kholudeeva <veronika-kholudeeva@mail.ru>
2023-03-16 18:28:28 +0300

    Create diary-app and added background and date

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header>
+      <p>#behappy</p>
+      <div className="icons">
+        <img className="icon" src="./images/i1.svg" />
+        <img className="icon" src="./images/i2.svg" />
+        <img className="icon" src="./images/i3.svg" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
